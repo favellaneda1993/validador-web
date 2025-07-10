f5914be275ecda102520df6be8e8dd340a83b75f
Franco Leonardo Avellaneda Gamero <favellaneda@grupoacp.local>
2025-07-10 11:20:28 -0500

    Primer commit desde local para despliegue
    B

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+
 from flask import Flask, render_template, request, jsonify
 from exportar_excel import generar_excel
+from flask import Flask, render_template, send_file
+from exportar_excel import generar_excel
+from detector_equipo import evaluar_equipo
+from detector_internet import evaluar_internet
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
 
 @app.route('/resultado', methods=['POST'])
 def resultado():
@@ -45,3 +52,17 @@ if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host='0.0.0.0', port=port)
+
+@app.route('/resultado')
+def resultado():
+    equipo = evaluar_equipo()
+    internet = evaluar_internet()
+    return render_template('resultado.html', equipo=equipo, internet=internet)
+
+@app.route('/descargar_excel')
+def descargar_excel():
+    archivo = generar_excel()
+    return send_file(archivo, as_attachment=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
